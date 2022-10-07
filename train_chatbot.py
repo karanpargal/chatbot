a397850662c515405b769f3f6eb9cef635270ddc
Pranav Gupta <88652820+pranavgupta2603@users.noreply.github.com>
2022-10-07 13:22:48 +0530

    Created .h5 file

--- train_chatbot.py
+++ train_chatbot.py
@@ -3,9 +3,9 @@ from nltk.stem import WordNetLemmatizer
 import json
 import pickle
 import numpy as np
-from keras.models import Sequential
-from keras.layers import Dense, Activation, Dropout
-from keras.optimizers import SGD
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, Dropout
+from tensorflow.keras.optimizers import SGD
 import random
 
 lemmatizer = WordNetLemmatizer()
